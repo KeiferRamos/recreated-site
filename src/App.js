24aa7a239adc062a4dd9e3955a3b83499463551d
KeiferRamos <keiferramos2001@gmail.com>
2022-06-06 14:10:23 +0800

    gg

--- src/App.js
+++ src/App.js
@@ -4,9 +4,17 @@ import useGlobalContext from "./context/globalcontext";
 import Navbar from "./components/navbar";
 import "./styles/app.css";
 import Footer from "./components/footer";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 function App() {
+  const navigate = useNavigate();
   const { screenWidth } = useGlobalContext();
+
+  useEffect(() => {
+    navigate("/");
+  }, []);
+
   return (
     <div className="App">
       {screenWidth <= 600 && <Navbar />}
